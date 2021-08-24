7197378396f27bb7d7ec58dd4de757cff7390d82
bbnopy <bbhanazono@outlook.com>
2021-08-24 19:02:49 +0300

    upd files

---
+++ Python_ALL/Flask/3flaskWebDevelopment2/part2/chapter10/listing10.07.ProfileEditForm/app/main/forms.py
@@ -0,0 +1,5 @@
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0,64)])
+    location = StringField('Location', validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
