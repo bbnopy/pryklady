a621350c6d328b855d02e711002485309cc3893c
bbnopy <bbhanazono@outlook.com>
2021-12-17 13:53:05 +0200

    html4 example 24 javascript

---
+++ HTML/HTML4/3StvorenniaWeb-storinokHTMLCSSJavaScript/part4/chapter24/my_script.js
@@ -0,0 +1,4 @@
+// file my_script.js
+function f1(){
+    alert("Dopomozhemo ditiam. Budemo pamiataty, shcho dity - kvity zhyttia. Ya zaproshuiu vas zaraz zhe zrobyty svoi vnesky s dopomohty ditiam.");
+}
